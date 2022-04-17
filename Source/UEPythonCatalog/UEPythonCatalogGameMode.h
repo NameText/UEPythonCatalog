ed2b583e7e9299cbc86eda4273146c8307188dfb
game_galy <ilovegamegaly@gmail.com>
2022-04-17 16:28:25 +0800

    feat:初始化工程

---
+++ Source/UEPythonCatalog/UEPythonCatalogGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEPythonCatalogGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUEPythonCatalogGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUEPythonCatalogGameMode();
+};
+
+
+
